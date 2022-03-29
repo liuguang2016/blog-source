d4e89a6e27033ad4468cb84bb4169ce1747b735f
liuguang <liuguang@shiqiao.com>
2022-03-29 14:15:47 +0800

    feat: update deploy sh

--- deploy.sh
+++ deploy.sh
@@ -7,16 +7,14 @@ set -e
 npm run build
 
 # 进入生成的文件夹
-cd dist/
+cd dist
 
-# git init
-# git checkout -b main
-git add .
+git init
+git checkout -b main
+git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io  填写你刚刚创建的仓库地址
-git remote add origin https://github.com/liuguang2016/liuguang2016.github.io.git
+git push -f https://github.com/liuguang2016/liuguang2016.github.io.git main
 
-git push -f origin  main:main
-
-cd ../
+cd -
